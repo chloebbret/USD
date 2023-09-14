47a005a8498ba057f67635ef085d168a7227d58d
Chloe <b.bernard@lyon.ort.asso.fr>
2023-09-14 09:40:23 +0200

    commit load

---
+++ public/pages/load.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/load.css">
+    <title>USD</title>
+</head>
+
+<body>
+<div class="loader-container" id="loader-container">
+    <div class="loader">
+        <img src="../img/logoBlanc.png" alt="Logo" id="image">
+    </div>
+</div>
+<div class="content" id="content">
+</div>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      // Masquer la page de chargement
+      document.getElementById("loader-container").style.opacity = "0";
+      setTimeout(function () {
+        // Afficher la page d'accueil
+        document.getElementById("loader-container").style.display = "none";
+        document.getElementById("content").style.display = "block";
+
+        // Agrandir le logo
+        document.getElementById("image").style.transform = "scale(4)"; // Vous pouvez ajuster l'échelle selon vos besoins
+
+        // Appeler la fonction de redirection après 1 seconde
+        setTimeout(function () {
+          redirectToAccueil();
+        }, 1000); // 1000 millisecondes = 1 seconde
+      }, 1000); // 1000 millisecondes = 1 seconde
+    }, 2000); // 2000 millisecondes = 2 secondes (ajustez la durée selon vos besoins)
+  });
+
+  function redirectToAccueil() {
+    window.location.href = "accueil.php";
+  }
+</script>
+</body>
+</html>
