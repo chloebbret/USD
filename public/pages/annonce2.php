610b9ecbb2fe71c086699e37bcae9eff0b24339d
Chloe <chloe022002@gmail.com>
2026-09-01 18:40:24 +0200

    Modif annonce 2

---
+++ public/pages/annonce2.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/body.css">
+    <link rel="stylesheet" href="../css/annonce.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Annonce</title>
+</head>
+<body>
+
+    <h1 class="h1Annonce">Reprise du championnat</h1>
+
+    <a href="accueil.php" class="retourAccueil">
+        <i class="fa-solid fa-arrow-left"></i> 
+        &nbsp Retour à l'accueil
+</a>
+
+    <div class="photoAnnonce">
+        <img src="../img/seniors1.jpg" alt="">
+    </div>
+
+    <p class="contenuAnnonce"> 
+        Retour sur les terrains pour notre équipe fanion ce Samedi 5 septembre 2026. Le match se jouera à Chambotte à 20h. 
+        <br> Notre équipe réserve reprendra, elle, le chemin des terrains la semaine prochaine.
+        <br> Pour nos féminines ainsi que nos jeunes, il faudra attendre courant septembre pour de nouveaux fouler les terrains.
+
+</body>
+</html>
